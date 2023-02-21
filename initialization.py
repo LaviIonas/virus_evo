d45b25ca4d0107f0994bce6f0622cbf281f72266
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2023-02-21 15:46:08 -0500

    mutation file + documentation

--- initialization.py
+++ initialization.py
@@ -2,6 +2,5 @@
 import numpy as np
 
 # function
-
 def initialize_virus_population (virus_length, virus_pop_size):
     return np.zeros((virus_length, virus_pop_size))
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # import
 import initialization
 
-# 
+# declerations
 virus_pop_size = 10
 virus_length = 6
 
+# EA
 virus_pop = initialization.initialize_virus_population(virus_length, virus_pop_size)
 print(virus_pop)
