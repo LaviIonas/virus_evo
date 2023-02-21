0e22ccb02f7cb69922f43dc6ac908217f123463b
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2023-02-21 15:48:33 -0500

    init mutation function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import
 import initialization
+import mutation
 
 # declerations
 virus_pop_size = 10
@@ -7,4 +8,6 @@ virus_length = 6
 
 # EA
 virus_pop = initialization.initialize_virus_population(virus_length, virus_pop_size)
+mutation.virus_mutation(virus_pop, virus_pop_size)
+
 print(virus_pop)
--- mutation.py
+++ mutation.py
@@ -1,3 +1,5 @@
 # import
 
 # function
+def virus_mutation(virus_pop, virus_pop_size):
+    return True
