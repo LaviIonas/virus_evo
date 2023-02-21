1353ca013c1abdd061f7e91e74f1dd2c8fb2f31a
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2023-02-21 15:50:46 -0500

    doc

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-# import
+# import library
+
+# import files
 import initialization
 import mutation
 
 # declerations
-virus_pop_size = 10
-virus_length = 6
+virus_pop_size = 10 # Number of Virus in Population
+virus_length = 6 # Size of the Virus
+virus_pop = [] # Set of all Virus'
 
-# EA
+# Evo Alg
+
+# INITIALIZE the virus population
 virus_pop = initialization.initialize_virus_population(virus_length, virus_pop_size)
+
+#
 mutation.virus_mutation(virus_pop, virus_pop_size)
 
 print(virus_pop)
