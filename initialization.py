6e6d35d1d8e4de91f8555a1b0e05504bd2e123e0
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2023-02-21 15:41:29 -0500

    var change

--- initialization.py
+++ initialization.py
@@ -3,7 +3,5 @@ import numpy as np
 
 # function
 
-def initialize_virus_population (virus_pop, virus_pop_size):
-    virus = np.zeros((virus_pop, virus_pop_size))
-
-    return virus
+def initialize_virus_population (virus_length, virus_pop_size):
+    return np.zeros((virus_length, virus_pop_size))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import initialization
 
 virus_pop_size = 10
-virus_pop = 10
+virus_length = 6
 
-virus = initialization.initialize_virus_population(virus_pop_size)
-print(virus)
+virus_pop = initialization.initialize_virus_population(virus_length, virus_pop_size)
+print(virus_pop)
