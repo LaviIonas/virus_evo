3db8c95dd4b3c0ec84873bfa7b79c17e8ad1d6c0
CyrilYMZ <cyrilyiming@gmail.com>
2023-02-21 21:21:51 -0500

    mutation

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ gen = 0
 
 # INITIALIZE the virus population
 virus_pop = initialization.initialize_virus_population(virus_length, virus_pop_size)
-
+mutation_rate = [0.4, 0.6]
 while gen < 10:
     new_virus_pop = [] # population of the new virus
 
     # MUTATE the virus
     for virus in virus_pop:
-        new_virus_pop.append(mutation.virus_mutation(virus))
+        new_virus_pop.append(mutation.virus_mutation(virus, mutation_rate))
 
     gen += 1 # Next Generation
 
-print(virus_pop)
+print(new_virus_pop)
