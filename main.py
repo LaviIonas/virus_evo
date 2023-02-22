050814e6d7c050e9ff7b7e6572961a35c8fdb4e9
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2023-02-21 21:13:36 -0500

    gen max

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ virus_rep_rate = [] # Set of all Reproduction Rates of each Virus
 # Evo Alg
 # ---------------
 
+# Set gen start
 gen = 0
+gen_max = 10
 
 # INITIALIZE the virus population
 virus_pop = initialization.initialize_virus_population(var.virus_length, var.virus_pop_size)
-# MUTATION INDEX
+# INITIALIZE the mutation index
 var.mutation_index = initialization.mutation_index()
 
-while gen < 10:
+while gen < gen_max:
     new_virus_pop = [] # population of the new virus
 
     # MUTATE the virus
