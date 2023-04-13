463062b7331db9f363e35713b7067fcf68b3419a
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2023-04-13 16:44:59 -0400

    basic mutation

--- mutation.py
+++ mutation.py
@@ -5,10 +5,10 @@ import global_var as var
 # function
 def virus_node_mutation(virus):
     if random.random() < var.virus_mut_rate:
-        r1 = random.randint(0,var.virus_length)
-        r2 = random.randint(0,var.virus_length)
-        print(virus[r1][r2])
-        var.swap[virus[r1][r2]]
+        r1 = random.randint(0,var.virus_length-1)
+        r2 = random.randint(0,var.virus_length-1)
+
+        virus[r1][r2] = 1 - virus[r1][r2]
 
 def virus_lp_muation(virus):
     return 0
