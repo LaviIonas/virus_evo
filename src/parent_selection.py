d07c0d3972b8aaa9b8defccbbe8fd214550e60d7
Priyank7299 <thakkarpriyank030@gmail.com>
2023-04-15 11:10:14 -0400

    Update parent_selection.py

--- src/parent_selection.py
+++ src/parent_selection.py
@@ -1,1 +1,10 @@
+import random
+import global_var as var
 
+
+def select_parents(var.virus_pop_size, fitness_scores):
+    total_fitness = sum(fitness_scores)
+    selection_probs = [score / total_fitness for score in fitness_scores]
+    parent1 = random.choices(var.virus_pop_size, weights=selection_probs)[0]
+    parent2 = random.choices(var.virus_pop_size, weights=selection_probs)[0]
+    return parent1, parent2
