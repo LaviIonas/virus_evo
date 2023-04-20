05c64645a2d90a7c622368eca52a8c3d50c3f0bc
Lavi Ionas <laviionas772@gmail.com>
2023-04-20 15:18:12 -0400

    main

--- src/main.py
+++ src/main.py
@@ -28,9 +28,9 @@ gen_max = 5
 # INITIALIZE
 virus_pop = initialization.initialize_virus_population()
 var.vaccine = initialization.initialize_vaccine()
-help.init_tree(virus_pop)
 
 # PRINT
+help.init_tree(virus_pop)
 print("Generation ZERO")
 help.print_virus_readable(virus_pop)
 var.tree.show()
@@ -39,9 +39,22 @@ var.tree.show()
 while gen < gen_max:
     gen += 1
 
-    
+    # VIRUS EVOLUTION
 
-    # MUTATION
+    # SELECT PARENTS
+    # ... n = 2
+    parents = []
+    # call cross over (parents)
+    # GENERATE OFFSPRING
+    # ... n = 2
+    offsping = []
+    # add offsping to population
+
+    # MUTATION OF POPULATION
     for i in virus_pop:
         mutation.natural_selection_mutation(i)
         help.update_virus_virality(i)
+
+    # VACCINE EVOLUTION
+
+    # ...
