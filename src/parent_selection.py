34890d6aec50459aca37f504da9915a9071115c8
Priyank7299 <thakkarpriyank030@gmail.com>
2023-04-21 19:00:00 -0400

    Update parent_selection.py

--- src/parent_selection.py
+++ src/parent_selection.py
@@ -2,9 +2,20 @@ import random
 import global_var as var
 
 
-def select_parents(virus_pop, fitness_scores):
+def select_parents(vaccine_pop, fitness_scores):
     total_fitness = sum(fitness_scores)
     selection_probs = [score / total_fitness for score in fitness_scores]
-    parent1 = random.choices(virus_pop, weights=selection_probs)[0]
-    parent2 = random.choices(virus_pop, weights=selection_probs)[0]
+    parent1 = random.choices(vaccine_pop, weights=selection_probs)[0]
+    parent2 = random.choices(vaccine_pop, weights=selection_probs)[0]
     return parent1, parent2
+
+# tournament selection
+def selection(pop, scores, k=4):
+ # first random selection
+ selection_ix = randint(len(pop))
+ for ix in randint(0, len(pop), k-1):
+
+ # check if better (e.g. perform a tournament)
+   if scores[ix] < scores[selection_ix]:
+   selection_ix = ix
+ return pop[selection_ix]
